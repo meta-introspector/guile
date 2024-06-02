7c9f3a335fc11ef75090d00fd36b28a41eefe9ae
mike dupont <mike.dupont@introspector.local>
2024-06-02 09:30:50 -0400

    start of introspector

---
+++ libguile/introspector.cc
@@ -0,0 +1,96 @@
+/*
+  c++ meta template programming magic for
+  * extracting of named entities
+  * extracting of contexts of graphs of morphisms surrounding those identifiers
+  * feeding to LLM
+ */
+
+#include <refl-cpp/refl.hpp>
+#include <iostream>
+
+template <
+  class CPPCompiler,
+  class SchemeCompiler,
+  class MachineArchitecture,
+  class OS,
+  class ConfigurationVariables,
+  class Guix,
+  class SelfReferencialTokens,
+  class LexicalIntrospection,
+  class Introspection,
+  class GuileIntrospectorSelf
+  > class GuileConfig {
+#include "config.h"  
+};
+
+
+#include "strings.h"
+#include "symbols.h"
+#include "vports.h"
+#include "numbers.h"
+#include "posix.h"
+#include "print.h"
+#include "scm.h"
+#include "intrinsics.h"
+#include "loader.h"
+//#include "vm-operationss.h"
+
+//REFL_TYPE(s )
+//REFL_END
+
+template <
+  class Ansible,
+  class Terraform,
+  class NeuralNet,
+  class LLM,
+  class Graph,
+  class Storage,
+  class Linux,
+  //  class GNUCompiler,
+  //  class LLVMCompiler,
+  class CPPCompiler,
+  class SchemeCompiler,
+  class SchemeVirtualMachine,
+  class MachineArchitecture,
+  class VirtualMachine,
+  class Guix,
+  class GuixBuildSystem,
+  class GuixBuildSystemPyProject,
+  class GuixBuildSystemGNU,
+  class Docker,
+  class Containerization,
+  class Proof,
+  class MonteCarloTreeSearch,
+  class TreeSearch,
+  class PolicyGraph,
+  class ValueGraph,
+  class Identifiers,
+  class Mathematics,
+  class Topology,
+  class AbstractAlgebra,
+  class AbstractDataClasss,
+  class AbstractSyntaxTrees,
+  class ClassDeclarations,
+  class ClassClassDeclarations,
+  class TemplateClassClassDeclarations,
+  class TemplateClassClassDeclarationParameters,
+  class SelfReferencialTemplateClassClassDeclarationParameters,
+  class SelfReferencialTokens,
+  class LexicalIntrospection,
+  class Introspection,
+  class Observe,
+  class Orient,
+  class Decide,
+  class Act,
+  class Life,
+  class LifeSimulation,
+  class UniverseSimulation,
+  class UniverseOfUniverses,
+  class Unimath,
+  class Researcher,
+  class Agent,
+  class Swarm,
+  class GuileIntrospectorSelf
+  > class GuileIntrospector {
+  
+};
---
+++ test.sh
@@ -0,0 +1,10 @@
+#/time/categories/build/guix/guile/libguile/.libs/guile
+
+/time/categories/build/guix/guile/libguile/.libs/guile \
+    -e "(@@ (guild) main)" \
+    --debug \
+    -s /time/categories/build/guix/guile/meta/guild compile \
+    --target=x86_64-pc-linux-gnu \
+    -L /time/categories/build/guix/guile/module -o ice-9/boot-9.go ./module/ice-9/boot-9.scm
+
+#["/time/categories/build/guix/guile/libguile/.libs/guile", "-e", "(@@ (guild) main)", "-s", "/time/categories/build/guix/guile/meta/guild", "compile", "--target=x86_64-pc-linux-gnu", "-W0", "-O1", "-L", "/time/categories/build/guix/guile/module", "-o", "ice-9/eval.go", "../module/ice-9/eval.scm"], 0x583707ce8f10 /* 79 vars */) = 0 ./libguile/guile 
